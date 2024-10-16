b6a708fbf1e769f2ba25b94edc3ebd8fa5c131fd
Sionra <farfadet.31@hotmail.fr>
2024-10-16 16:53:43 +0200

    Testing Figlet

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,10 +1,13 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(FigletFont.convertOneLine("Patate"));
     }
 }
