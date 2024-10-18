72ae4eef278a4c101a275523abafeaf00b5e077d
Sionra <farfadet.31@hotmail.fr>
2024-10-18 13:45:27 +0200

    Maven build can work

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -2,9 +2,6 @@ package dev;
 
 import com.github.lalyos.jfiglet.FigletFont;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
         System.out.println(FigletFont.convertOneLine("App"));
